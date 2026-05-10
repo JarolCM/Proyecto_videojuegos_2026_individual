732c35501b5080c11a533b3a7c9cc7de6ffbf6a3
JarolCM <jarol.carrascal@gmail.com>
2026-05-09 22:49:22 -0500

    mecanica de distraccion
    - ahora spike detecta los sonidos generados en un area y va a investigar
    - creacion de botones para menu principal para usar en un futuro
    - ajuste en duracion de animaciones de max
    - mecanica de maullido y animacion de tirar objetos agregada
    - señal global para sonidos agregada

--- escenas/max_gato/max.gd
+++ escenas/max_gato/max.gd
@@ -2,19 +2,20 @@ extends CharacterBody2D
 
 #signal max_muerto
 
+
 @export var animacion: AnimatedSprite2D
 @export var area_2d: Area2D
 
 
-const _SPEED = 100.0
-const _JUMP_VELOCITY = -350.0
+const _SPEED = 85.0
+const _JUMP_VELOCITY = -300.0
 var _muerto: bool
 
 func _ready():
 	area_2d.body_entered.connect(_on_area_2d_body_entered)
 
 
-func _physics_process(delta: float) -> void:	
+func _physics_process(delta: float) -> void:
 	# Gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -22,29 +23,62 @@ func _physics_process(delta: float) -> void:
 	# Input salto
 	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		velocity.y = _JUMP_VELOCITY
+	
+	
+	# Bloqueo de acciones
+	var bloqueado := (
+		(animacion.animation == "maullido" or animacion.animation == "tirar_objeto")
+		and animacion.is_playing()
+	)
+	
+	# Movimiento
+	if not bloqueado:
+		var direction := Input.get_axis("izquierda", "derecha")
 
-	# Input direccional
-	var direction := Input.get_axis("izquierda", "derecha")
+		if direction < 0:
+			velocity.x = direction * _SPEED
+			animacion.flip_h = true
 
-	#Movimiento
-	if direction < 0:
-		velocity.x = direction * _SPEED
-		animacion.flip_h = true
-	elif direction > 0:
-		velocity.x = direction * _SPEED
-		animacion.flip_h = false
+		elif direction > 0:
+			velocity.x = direction * _SPEED
+			animacion.flip_h = false
+
+		else:
+			velocity.x = move_toward(velocity.x, 0, _SPEED)
+	
 	else:
-		velocity.x = move_toward(velocity.x, 0, _SPEED)
-		
+		# Frenar mientras hace acciones
+		velocity.x = 0
 	move_and_slide()
 	
-	#animaciones
+	# No interrumpir animaciones especiales
+	if bloqueado:
+		return
+	
+	#animaciones y acciones
+	#para que no se interrumpa las animaciones
+	if (animacion.animation == "maullido" or animacion.animation == "tirar_objeto") and animacion.is_playing():
+		return
+
+	if Input.is_action_just_pressed("maullido"):
+		animacion.play("maullido")
+		SeñalesGlobales.sonido_generado.emit(global_position)
+		print("¡Miau!")
+		return
+	
+	if Input.is_action_just_pressed("interactuar"):
+		animacion.play("tirar_objeto")
+		print("Interactuando...")
+		return
+	
+	# Animaciones de movimiento
 	if !is_on_floor():
 		animacion.play("saltar")
 	elif velocity.x != 0:
 		animacion.play("correr")
 	else:
 		animacion.play("idle")
+	
 
 
 #Funcion colision de daño (quitar 1 de los 2)
--- escenas/spike_perro/spike.gd
+++ escenas/spike_perro/spike.gd
@@ -1,18 +1,24 @@
 extends CharacterBody2D
 
 @export var animacion: AnimatedSprite2D
-@export var area_2d: Area2D
+@export var area_vision: Area2D
 @export var area_daño: Area2D
+@export var area_escucha: Area2D
 
-
-const SPEED = 80.0
+const SPEED = 90.0
 
 var jugador_max: CharacterBody2D = null
+var objetivo_sonido: Vector2
+var investigando_sonido := false
 
 func _ready():
-	area_2d.body_entered.connect(_on_area_2d_body_entered)
-	area_2d.body_exited.connect(_on_area_2d_body_exited)
+	area_vision.body_entered.connect(_on_area_vision_body_entered)
+	area_vision.body_exited.connect(_on_area_vision_body_exited)
 	area_daño.body_entered.connect(_on_area_daño_body_entered)
+	area_escucha.body_entered.connect(_on_area_escucha_body_entered)
+	
+	SeñalesGlobales.sonido_generado.connect(_on_sonido_escuchado)
+	
 
 func _physics_process(delta: float) -> void:
 	#Gravedad
@@ -20,16 +26,25 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	#Movimiento
+	#Ir a jugador
 	if jugador_max:
-		var direction = (jugador_max.global_position - global_position).normalized()
-
-		velocity.x = direction.x * SPEED
-
-		if velocity.x < 0:
-			animacion.flip_h = true
-		else:
-			animacion.flip_h = false
+		var direction = sign(jugador_max.global_position.x - global_position.x)
+		velocity.x = direction * SPEED
+		investigando_sonido = false
 	
+	# Ir a sonido
+	elif investigando_sonido:
+		var direction = sign(objetivo_sonido.x - global_position.x)
+		velocity.x = direction * SPEED
+
+		if abs(global_position.x - objetivo_sonido.x) < 10:
+			investigando_sonido = false
+			velocity.x = 0
+		
+		if is_on_wall():
+			investigando_sonido = false
+	
+	# Sin objetivos
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -37,18 +52,19 @@ func _physics_process(delta: float) -> void:
 	
 	#animaciones
 	if velocity.x != 0:
+		animacion.flip_h = velocity.x < 0
 		animacion.play("correr")
 	else:
 		animacion.play("idle")
 
 #Cuando max entra en el campo de vision de spike
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_vision_body_entered(body: Node2D) -> void:
 	if body.is_in_group("max"):
 		jugador_max = body
 		print("¡Max detectado!")
 
 #Cuando max sale del campo de vision de spike
-func _on_area_2d_body_exited(body: Node2D) -> void:
+func _on_area_vision_body_exited(body: Node2D) -> void:
 	if body.is_in_group("max"):
 		jugador_max = null
 		print("Max salió del rango")
@@ -56,6 +72,26 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 #Cuando spike choca con max
 func _on_area_daño_body_entered(body: Node2D) -> void:
-	if body.is_in_group("max"):
-		print("game over")
+	#if body.is_in_group("max"):
+		#print("game over")
 		#Por poner "game over"
+	pass
+
+#Cuando entra al area de escucha?
+func _on_area_escucha_body_entered(body: Node2D) -> void:
+	pass
+	
+func _on_sonido_escuchado(posicion_sonido: Vector2):
+	var shape = area_escucha.get_node("CollisionShape2D").shape
+	var largo = shape.size
+	
+	# Distancia entre Spike y el sonido
+	var local_pos = area_escucha.to_local(posicion_sonido)
+	var half_size = shape.size * 0.5
+	
+	if abs(local_pos.x) <= half_size.x and abs(local_pos.y) <= half_size.y:
+		# Solo investiga si no está viendo a Max actualmente
+		if jugador_max == null:
+			objetivo_sonido = posicion_sonido
+			investigando_sonido = true
+			print("¡Spike escuchó algo en: ", posicion_sonido, "!")
